e671bbdc642168a9a05ff51de36856b3a4cc11b4
chidopower <43185265+chidopower@users.noreply.github.com>
2020-04-15 17:18:21 -0600

    Add files via upload

--- testp5/sketch.js
+++ testp5/sketch.js
@@ -1,3 +1,61 @@
+/*
+//--------------------------------------------
+let rectX = 0;
+//let fr = 60; //starting FPS
+let clr;
+let speed = 1;
+
+function setup() {
+  background(200);
+  //frameRate(fr); 
+  clr = color(255, 0, 0);
+}
+
+function draw() {
+  background(200);
+  rectX = rectX + speed*(deltaTime/100);
+  
+  console.log(deltaTime, rectX);
+
+  if (rectX >= width) {
+      clr = color(255, 0, 0);
+      //frameRate(fr); 
+    rectX = 0;
+  }
+  fill(clr);
+  rect(rectX, 40, 20, 20);
+}
+*/
+
+
+
+/*
+//--------------------------------------------
+function setup() {
+  // We are still calling createCanvas like in the past, but now 
+  // we are storing the result as a variable. This way we can 
+  // call methods of the element, to set the position for instance.
+  let canvas = createCanvas(800, 480);
+
+  // Here we call methods of each element to set the position 
+  // and id, try changing these values.
+  // Use the inspector to look at the HTML generated from this 
+  // code when you load the sketch in your browser.
+  canvas.position(300, 50);
+  canvas.class("lemon");
+}
+
+function draw() {
+  // These commands are applied to the graphics canvas as normal.
+  background(220, 180, 200);
+  ellipse(width/2, height/2, 100, 100);
+  ellipse(width/4, height/2, 50, 50);
+}
+*/
+
+
+/*
+//--------------------------------------------
 var x =   [];
 var y =   [];
 var xSpeed =   [];
@@ -8,12 +66,14 @@ var g =   [];
 var b =   [];
 
 function setup() {
-  createCanvas(800, 480);
+
+  let canvas = createCanvas(800, 480);
+  canvas.class("lemon");
 }
 
 function draw() {
 
-  background(200);
+  background("lemon");
 
   for (i = 0; i < x.length; i++) {
 
@@ -42,13 +102,14 @@ function mousePressed() {
   g = append(g, random(256));
   b = append(b, random(256));
 }
+*/
 
 
-/*
+//--------------------------------------------
 let x = 200;
 let y = 100;
-let xSpeed = 3;
-let ySpeed = 3;
+let xSpeed = 30;
+let ySpeed = 30;
 
 function setup() {
   createCanvas(800, 600);
@@ -58,8 +119,8 @@ function draw() {
   background(64);
   ellipse(x, y, 100, 100);
 
-  x += xSpeed;
-  y += ySpeed;
+  x += xSpeed*(deltaTime/100);
+  y += ySpeed*(deltaTime/100);
   
   if (x-50 < 0 || x > width-50) {
     xSpeed *= -1;
@@ -69,4 +130,3 @@ function draw() {
     ySpeed *= -1;
   }
 }
-*/
