392e31c5eb8cc32cc8c9d0732c3219e20dce1a0a
chidopower <43185265+chidopower@users.noreply.github.com>
2020-04-17 21:42:58 -0600

    Add files via upload

--- testp5/sketch.js
+++ testp5/sketch.js
@@ -1,140 +1,205 @@
-/*
-//--------------------------------------------
-let rectX = 0;
-//let fr = 60; //starting FPS
-let clr;
-let speed = 1;
+//--------------------------- BIRD VARS
+var bx;
+var by;
+var bspx; //speed in x
+var bspy;
+var br; //radius
 
-function setup() {
-  background(200);
-  //frameRate(fr); 
-  clr = color(255, 0, 0);
-}
+//--------------------------- PIPES VARS
+var px=[];
+var py0=[];
+var py1=[];
+var py2=[];
+var py3=[];
+var pw=[];
+var pspx; //speed pipes in x
+var pspy;
+const Npipes = 4;
+var pgapx; //gap beetwen pipes
+var pgapy;
+var pR=[];
+var pG=[];
+var pB=[];
 
-function draw() {
-  background(200);
-  rectX = rectX + speed*(deltaTime/100);
-  
-  console.log(deltaTime, rectX);
+//--------------------------- GRAL VARS
+var frame;
+var time;
+var spawn_t_old=0;
+var spawn_t_now=0;
+var spawn_dt;
+var FPS;
+const G = 0.5; //gravity
+const FR = 60;
 
-  if (rectX >= width) {
-      clr = color(255, 0, 0);
-      //frameRate(fr); 
-    rectX = 0;
-  }
-  fill(clr);
-  rect(rectX, 40, 20, 20);
-}
-*/
-
-
-
-/*
-//--------------------------------------------
-function setup() {
-  // We are still calling createCanvas like in the past, but now 
-  // we are storing the result as a variable. This way we can 
-  // call methods of the element, to set the position for instance.
-  let canvas = createCanvas(800, 480);
-
-  // Here we call methods of each element to set the position 
-  // and id, try changing these values.
-  // Use the inspector to look at the HTML generated from this 
-  // code when you load the sketch in your browser.
-  canvas.position(300, 50);
-  canvas.class("lemon");
-}
-
-function draw() {
-  // These commands are applied to the graphics canvas as normal.
-  background(220, 180, 200);
-  ellipse(width/2, height/2, 100, 100);
-  ellipse(width/4, height/2, 50, 50);
-}
-*/
-
-
-/*
-//--------------------------------------------
-var x =   [];
-var y =   [];
-var xSpeed =   [];
-var ySpeed =   [];
-var size =   [];
-var r =   [];
-var g =   [];
-var b =   [];
-
+//-----------------------------------------SETUP
 function setup() {
 
-  let canvas = createCanvas(800, 480);
-  canvas.class("lemon");
+	createCanvas(800, 480);
+	background("black");
+	frameRate(FR);
+	frame = 0;
+	time = 0;
+
+	//------------------------------------ the bird params
+	bx = width/4;
+	by = height/2;
+	br = height/24;
+	bspx = 0;
+	bspy = 0;
+
+	//----------------------------------- pipes params
+	pgapx = 8*br; //gap beetwen pipes
+	pgapy = 6*br;
+	pspx = 0.2; //speed pipes 
+	pipe_spawn(width);  //first pipe
+	pipe_spawn(width+width*(1/3));  //second pipe
+	pipe_spawn(width+width*(2/3));  //
+
 }
 
+//------------------------- pipe_spawn()
+function pipe_spawn(X){
+	
+	let y0 = 0;
+	let y1 = random(2*br,height/2);
+	let y2 = y1 + pgapy;
+	let y3 = width - y2;
+	
+	px = append(px, X);
+	py0 = append(py0, y0);
+	py1 = append(py1, y1);
+	py2 = append(py2, y2);
+	py3 = append(py3, y3);
+	pw = append(pw, 4*br);	
+	
+	pR = append(pR, random(256) );
+	pG = append(pG, random(256) );
+	pB = append(pB, random(256) );
+	
+	//spawn_t_old = spawn_t_now;
+	//spawn_t_now = time/1000;
+	//spawn_dt = spawn_t_now - spawn_t_old;
+	
+}
+
+//-----------------------------------------DRAW
 function draw() {
+	
+	background("black");
 
-  background("lemon");
+	//------------------------update & draw pipes
+	
+	//if(time/
+	//pipe_spawn();
+	
+	for(let i=0; i<px.length; i++){
+		
+		if(px[i]+pw[i] < 0){
+			
+			px[i] = width;
+			pR[i] = random(256);
+			pG[i] = random(256);
+			pB[i] = random(256);
+		}
+		
+		px[i] -= pspx*deltaTime;
+		
+	}
+	
+	for(let i=0; i<px.length; i++){
+		
+		if(i===0)
+			fill("white");
+		else
+			fill(pR[i], pG[i], pB[i]);
+		
+		rect(px[i], py0[i], pw[i], py1[i]);	
+		rect(px[i], py2[i], pw[i], py3[i]);	
+	}
+	
+	console.log(px.length, time);
 
-  for (i = 0; i < x.length; i++) {
+	//-----------------------update & draw text
+	textSize(20);
+	fill("green")
+	text("FPS: "+frame/(time/1000), width-120, 30);	
+	text("frame: "+frame, width-120, 60);
+	text("time: "+floor(time/1000), width-120, 90);	
+	text("("+floor(bx)+", "+floor(by)+")", width-120, 120);
+	text("bspy: "+floor(bspy), width-120, 150);
 
-    x[i] += xSpeed[i];
-    if (x[i]-size[i]/2.0 < 0 || x[i] > width-size[i]/2.0) {
-      xSpeed[i] *= -1;
-    }
+	//------------------------ update & draw bird
 
-    y[i] += ySpeed[i];
-    if (y[i]-size[i]/2.0 < 0 || y[i] > height-size[i]/2.0) {
-      ySpeed[i] *= -1;
-    }
+	bspy = bspy + G;
+	by = by + bspy;
+	
+	if(by+br > height){
+		by = height - br;
+		bspy = 0;
+	}
+	
+	if(by-br <= 0){
+		by = br;
+		bspy = 0;
+	}
+	
+	fill("white");
+	circle(bx, by, 2*br);
 
-    fill(r[i], g[i], b[i]);
-    ellipse(x[i], y[i], size[i], size[i]);
-  }
+
+	//--------------------------- update time
+	frame++;
+	time += deltaTime;
+
 }
+
+//-----------------------------------------INPUT
 
 function mousePressed() {
-  x = append(x, mouseX);
-  y = append(y, mouseY);
-  xSpeed = append(xSpeed, random(-5, 5));
-  ySpeed = append(ySpeed, random(-5, 5));
-  size = append(size, random(80, 100));
-  r = append(r, random(256));
-  g = append(g, random(256));
-  b = append(b, random(256));
-}
-*/
-
-
-//--------------------------------------------
-let x = 50;
-let y = 50;
-let xSpeed = 30;
-let ySpeed = 30;
-
-function setup() {
-  createCanvas(800, 480);
-  
+	bspy = -6;
 }
 
+function keyPressed() {
+	if (keyCode === 32) {
+		bspy = -6;
+	}
+}
+
+/*
 function mousePressed() {
-  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-    let fs = fullscreen();
-    fullscreen(!fs);
-  }
-}
+	x = append(x, mouseX);
+	y = append(y, mouseY);
+	cl = append(cl, "white");
+	xSpeed = append(xSpeed, 0.1);
+	ySpeed = append(ySpeed, 0.1);
+	R = append(R, 15);
+}*/
 
-function draw() {
-  background(64);
-  ellipse(x, y, 100, 100);
+/*  for (i = 0; i < x.length; i++) {
+	  
+		x[i] += xSpeed[i]*deltaTime;
+		y[i] += ySpeed[i]*deltaTime;
 
-  x += xSpeed*(deltaTime/100);
-  y += ySpeed*(deltaTime/100);
-  
-  if (x-50 < 0 || x > width-50) {
-    xSpeed *= -1;
-  }
+		if (x[i]-R[i] < 0) {
+			xSpeed[i] *= -1;
+			x[i]=R[i];
+		}
 
-  if (y-50 < 0  || y > height-50) {
-    ySpeed *= -1;
-  }
-}
+		if (x[i] > width-R[i]) {
+			xSpeed[i] *= -1;
+			x[i]=width-R[i];
+		}
+
+		if (y[i]-R[i] < 0 ) {
+			ySpeed[i] *= -1;
+			y[i]=R[i];
+		}
+
+		if ( y[i] > height-R[i]) {
+			ySpeed[i] *= -1;
+			y[i]=height-R[i];
+		}
+
+  }*/
+
+
