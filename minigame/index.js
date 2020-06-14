cdd3c1f4f4dde9ddb12d45c4ca82fc7b50c94b5d
chidopower <43185265+chidopower@users.noreply.github.com>
2020-06-14 13:17:02 -0600

    Add files via upload

--- minigame/index.js
+++ minigame/index.js
@@ -13,7 +13,10 @@ var keycounter = 0;
 var state;
 
 var hero;
-var mob;
+var mobs;
+var apple;
+var score;
+var opt;
 
 function init() {
 	
@@ -24,19 +27,9 @@ function init() {
 	canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border: 0px solid blue";
 
 	t_old = performance.now();  
-	frame = 0;
 	
-	hero = new Hero(X0+2*PIXEL, Y1-2*PIXEL, 2*PIXEL, 2*PIXEL);
-	hero.lw = 1;
-	hero.lc = "black";
-	hero.fc = "yellow";
-
-	mob = new Mob(X0+8*PIXEL, Y0, PIXEL, PIXEL);
-	mob.lw = 1;
-	mob.lc = "black";
-	mob.fc = "red";
-
-	state = "playing";
+	frame = 0;
+	state = "play";
 	
 	requestAnimationFrame(loop);	
 
@@ -98,11 +91,12 @@ function loop() {
 	t_old   = t_now;
 	fps 	+= 1000/dt;
 	
-	if(frame%60===0){
-		fps = fps/60;
-		console.log("fps:", Math.floor(fps));
-		fps = 0;
-	}
+	// if(frame%60===0){
+		// fps = fps/60;
+		// console.log("fps:", Math.floor(fps));
+		//console.log(frame);
+		// fps = 0;
+	// }
 	
 	states();
 
@@ -111,16 +105,28 @@ function loop() {
 
 init();
 
-window.addEventListener('click', click, false);
-function click(e){
+window.addEventListener('mousedown', mousedown, false);
+function mousedown(e){
 	
 	click_counter++;
+	console.log(click_counter);
 	
 	mouse = {x: e.clientX, y: e.clientY};
 	
 	console.log("click", mouse.x, mouse.y);
 	
 }
+
+// window.addEventListener('click', click, false);
+// function click(e){
+	
+	// click_counter++;
+	
+	// mouse = {x: e.clientX, y: e.clientY};
+	
+	// console.log("click", mouse.x, mouse.y);
+	
+// }
 
 window.addEventListener('keydown', keydown, false);
 function keydown(e) {
@@ -144,27 +150,108 @@ function keyup(e) {
 
 function states(){
 	
+	if(state==="play"){
+		
+		opt = { speed: PIXEL/2/2/2,
+				gravity: PIXEL/2/2
+		}
+		
+		hero = new Hero(X0+2*PIXEL, Y1-2*PIXEL, 2*PIXEL, 2*PIXEL);
+		hero.lw = 0;
+		hero.lc = "black";
+		hero.fc = "blue";
+		hero.dx = opt.speed;
+		
+		apple = new Apple(X0, Y0, PIXEL, PIXEL);
+		apple.fc = "red";
+		apple.lc = "black";
+		apple.lw = 0;
+		apple.dy = opt.speed;
+		
+		apple_respawn();
+		
+		mobs = [];
+
+		mobs_spawn();
+		
+		score = 0;
+		
+		state = "playing";
+		
+	}
+	
+	
 	if(state==="playing"){
 		
-		
-		
-		if(frame%15===0){
+		if(frame%1===0){
+			
+			//clear canvas
 			
 			ctx.fillStyle = "black";
 			ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+			ctx.fillStyle = "gray";
+			ctx.fillRect(X0, Y0, screen_w, screen_h);
 			
-			draw_screen();
+			
+			//update
 			
 			hero_update();
-			mob_update();
+			
+			apple_update(apple);
+			
+			for(let i=0; i<mobs.length; i++)
+				mob_update(mobs[i]);
+        
+			
+			//collisions
+			
+			if(coll_rect_rect(hero, apple)){
+				apple_respawn();
+				score++;
+				console.log("SCORE: ", score);
+				mobs_spawn();
+			}
+			
+			for(let i=0; i<mobs.length; i++){
+				
+				if(coll_rect_rect(hero, mobs[i])){
+					
+					state = "play";
+					console.log("GAME OVER");
+					
+				}
+				
+			}
+			
+			
+
+			
+			//draw
 			
 			draw_rect(hero);
-			draw_rect(mob);
+			draw_rect(apple);
+			for(let i=0; i<mobs.length; i++)
+				draw_rect(mobs[i]);
+			
+			//draw_screen();
+			
+			ctx.fillStyle = "black";
+			ctx.fillRect(X1, 0, window.innerWidth, window.innerHeight);
+			ctx.fillRect(0, 0, X0, window.innerHeight);
+			
 		}
 
 	}
 	
 }
+
+class MyHero {
+	constructor(x, y){
+		this.alto = x;
+		this.ancho = y;
+	}
+}
+
 
 function Hero(x, y, w, h){
 	
@@ -174,79 +261,87 @@ function Hero(x, y, w, h){
 	this.h = h;
 	this.fc = "gray";
 	this.lc = "black";
-	this.lw = 1;	
-	this.dx = PIXEL;
-	this.dy = PIXEL;
+	this.lw = 0;	
+	this.dx = 0;
+	this.dy = 0;
+	this.jumping = true;
+	this.jump = 0;
 	
 }
 
 function hero_update(){
 	
+	hero.dy += opt.gravity;
+	
 	hero.x += hero.dx;
 	hero.y += hero.dy;
 	
-	if(hero.x <= X0){
-		hero.x = X0;
+	// console.log(hero.x);
+	
+	if(hero.x< X0){
+		// hero.x = X1;
 		hero.dx *= -1;
 	}
 
-	if(hero.x >= X1 - hero.w){
-		hero.x = X1 - hero.w;
+	if(hero.x+hero.w > X1){
+		
 		hero.dx *= -1;	
+		hero.x = X1 - hero.w;
 	}
 
-	if(hero.y <= Y0){
-		hero.y = Y0;
-		hero.dy *= -1;
-	}
-
-	if(hero.y >= Y1 - hero.h){
-		hero.y = Y1 - hero.h;
-		hero.dy *= -1;	
+	if(hero.y + hero.h >= Y1){
+		hero.y  = Y1 - hero.h;
+		hero.jumping = false;
+		// hero.jump = 0;
+		hero.dy = 0;
+		if(hero.dx>0)
+			hero.dx = opt.speed;
+		else
+			hero.dx = -opt.speed;
 	}
 	
-	if(mouse.x >= X1 && hero.dx > 0 && click_counter===1){
-		hero.dx *= -1;
+	if(mouse.x >= X1 && click_counter===1 ){
+		if(hero.dx<0)
+			hero.dx *= -1;
 		click_counter = 0;
 	}
 
-	if(mouse.x >= X1 && hero.dx < 0 && click_counter===1){
-		hero.dx *= -1;
-		click_counter = 0;
-	}	
-
-	if(mouse.x <= X0 && hero.dy > 0 && click_counter===1){
-		hero.dy *= -1;
+	if(mouse.x <= X0 && click_counter===1){
+		if(hero.dx>0)
+			hero.dx *= -1;		
 		click_counter = 0;
 	}
 
-	if(mouse.x <= X0 && hero.dy < 0 && click_counter===1){
-		hero.dy *= -1;
+	if(mouse.x > X0 && mouse.x < X1 && click_counter===1){
+		hero.dy -= hero.h;
+		if(hero.dx>0)
+			hero.dx = 4*opt.speed;
+		else
+			hero.dx = -4*opt.speed;
+		hero.jumping = true;
 		click_counter = 0;
-	}	
-//--
-	if(key.ArrowRight && hero.dx > 0 && keycounter===1){
-		hero.dx *= -1;
-		key.ArrowRight = false;
-
 	}
 
-	if(key.ArrowRight && hero.dx < 0 && keycounter===1){
+	if(key.ArrowRight && keycounter===1 && hero.dx<0){
 		hero.dx *= -1;
 		key.ArrowRight = false;
-	}	
-
-	if(key.ArrowLeft && hero.dy > 0 && keycounter===1){
-		hero.dy *= -1;
-		key.ArrowLeft = false;
 	}
-
-	if(key.ArrowLeft && hero.dy < 0 && keycounter===1){
-		hero.dy *= -1;
-		key.ArrowLeft = false;
-
+	
+	if(key.ArrowLeft && keycounter===1 && hero.dx>0){
+		hero.dx *= -1;
+		key.ArrowRight = false;
 	}	
 	
+	if(key.ArrowUp && !hero.jumping && keycounter===1){
+		hero.dy -= hero.h;
+		if(hero.dx>0)
+			hero.dx = 4*opt.speed;
+		else
+			hero.dx = -4*opt.speed;
+		hero.jumping = true;
+		key.ArrowRight = false;
+	}
+
 }
 
 function hero_update_(){
@@ -294,6 +389,64 @@ function hero_update_(){
 		hero.x = X1 - hero.w;
 }
 
+
+
+function coll_rect_rect(A, B){
+
+	if (A.x < B.x + B.w &&
+	   A.x + A.w > B.x &&
+	   A.y < B.y + B.h &&
+	   A.h + A.y > B.y){
+	   return true;
+	}else{
+		return false;
+	}
+}
+
+function Apple(x, y, w, h){
+	
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.fc = "gray";
+	this.lc = "black";
+	this.lw = 1;
+	this.dy = 1;	
+	
+}
+
+function apple_respawn(){
+	
+	
+	while(1){
+		apple.x = X0 + irand(1, N-1)*PIXEL;
+		apple.y = Y0 + apple.h;;
+		
+		if(!coll_rect_rect(hero, apple))
+			break;
+		
+	}
+
+	
+
+}
+
+function apple_update(o){
+	
+	o.y += o.dy;
+	
+	if((o.y + o.h) >= Y1){
+		o.dy *= -1;
+	}
+
+	if((o.y ) <= Y0){
+		o.dy *= -1;
+	}
+
+}
+
+
 function Mob(x, y, w, h){
 	
 	this.x = x;
@@ -303,59 +456,85 @@ function Mob(x, y, w, h){
 	this.fc = "gray";
 	this.lc = "black";
 	this.lw = 1;
-	this.dx = PIXEL;
-	this.dy = PIXEL;
+	this.dx = 1;
+	this.dy = 1;
 	
 }
 
-function mob_update(){
+function mobs_spawn(){
 	
-	mob.x += mob.dx;
-	mob.y += mob.dy;
+	let k;
 	
-	if(mob.x <= X0){
-		mob.x = X0;
-		mob.dx *= -1;
+	if(mobs.length===0)
+		k = 0;
+	else
+		k = mobs.length;
+	
+	mobs.push( new Mob(X0+irand(1,N-1)*PIXEL, Y0+irand(1, N/2)*PIXEL, PIXEL, PIXEL) );
+	
+	if(irand(1,2)===1)
+		mobs[k].dx *= -1;
+	if(irand(1,2)===1)
+		mobs[k].dy *= -1;
+	
+	mobs[k].lw = 0;
+	mobs[k].lc = "black";
+	mobs[k].fc = "black";
+	mobs[k].dx = opt.speed;
+	mobs[k].dy = opt.speed;
+	
+	//console.log(mobs[0]);
+
+}
+
+function mob_update(o){
+	
+	o.x += o.dx;
+	o.y += o.dy;
+	
+	if(o.x <= X0){
+		o.x = X0;
+		o.dx *= -1;
 	}
 
-	if(mob.x >= X1 - mob.w){
-		mob.x = X1 - mob.w;
-		mob.dx *= -1;	
+	if(o.x >= X1 - o.w){
+		o.x = X1 - o.w;
+		o.dx *= -1;	
 	}
 
-	if(mob.y <= Y0){
-		mob.y = Y0;
-		mob.dy *= -1;
+	if(o.y <= Y0){
+		o.y = Y0;
+		o.dy *= -1;
 	}
 
-	if(mob.y >= Y1 - mob.h){
-		mob.y = Y1 - mob.h;
-		mob.dy *= -1;	
+	if(o.y >= Y1 - o.h){
+		o.y = Y1 - o.h;
+		o.dy *= -1;	
 	}
 	
 }
 
 function draw_screen(){
 	
-	ctx.fillStyle = "black";
+	ctx.fillStyle = "gray";
 	ctx.fillRect(X0, Y0, screen_w, screen_h);
 
-	for(let i=0; i<N+1; i++){
-		ctx.beginPath();
-		ctx.moveTo(X0+i*PIXEL, Y0);
-		ctx.lineTo(X0+i*PIXEL, Y1);
-		ctx.strokeStyle = "green";
-		ctx.lineWidth = 1;
-		ctx.stroke();	
-	}
+	// for(let i=0; i<N+1; i++){
+		// ctx.beginPath();
+		// ctx.moveTo(X0+i*PIXEL, Y0);
+		// ctx.lineTo(X0+i*PIXEL, Y1);
+		// ctx.strokeStyle = "black";
+		// ctx.lineWidth = 1;
+		// ctx.stroke();	
+	// }
 
-	for(let i=0; i<N+1; i++){
-		ctx.beginPath();
-		ctx.moveTo(X0, Y0+i*PIXEL);
-		ctx.lineTo(X1, Y0+i*PIXEL);
-		ctx.strokeStyle = "green";
-		ctx.stroke();	
-	}
+	// for(let i=0; i<N+1; i++){
+		// ctx.beginPath();
+		// ctx.moveTo(X0, Y0+i*PIXEL);
+		// ctx.lineTo(X1, Y0+i*PIXEL);
+		// ctx.strokeStyle = "black";
+		// ctx.stroke();	
+	// }
 
 }
 
@@ -373,4 +552,8 @@ function draw_rect(r){
 		ctx.stroke();	
 	}
 	
+}
+
+function irand(min,max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
