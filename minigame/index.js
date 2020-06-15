35a858e829a9eeeced7f7aecd08e7cd4fac62004
chidopower <43185265+chidopower@users.noreply.github.com>
2020-06-15 01:04:42 -0600

    Add files via upload

--- minigame/index.js
+++ minigame/index.js
@@ -7,6 +7,7 @@ var dt = 0;
 var frame = 0;
 var fps = 0;
 var mouse = {x: 1, y:1};
+var touch = {left: false, right: false};
 var click_counter = 0;
 var key = {};
 var keycounter = 0;
@@ -21,7 +22,6 @@ var opt;
 function init() {
 	
 	get_hxw();
-
 	ctx.canvas.width  = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
 	canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border: 0px solid blue";
@@ -29,7 +29,7 @@ function init() {
 	t_old = performance.now();  
 	
 	frame = 0;
-	state = "play";
+	state = "welcome";
 	
 	requestAnimationFrame(loop);	
 
@@ -38,37 +38,41 @@ function init() {
 var orient_now;
 var N;
 var screen_h, screen_w;
-var X0, Y0, X1, Y1;
+var X0, Y0, X1, Y1, XC, YC;
 
 function get_hxw(){
 	
+	N = 20;
+	
 	if(window.innerWidth>window.innerHeight){
-		
 		orient_now = "landscape";
-		
-		N = 20;
-		
 		PIXEL = Math.floor(window.innerHeight/N);
-		while(1){
-			if(PIXEL%2===0)
-				break;
-			else
-				PIXEL--;			
-		}
-		
-		PIXEL = PIXEL/2;
-		N = 2*N;
-		
-		screen_w = N*PIXEL;
-		screen_h = N*PIXEL;
-		
-		X0 = Math.floor( (window.innerWidth - screen_w)/2 );
-		Y0 = Math.floor( (window.innerHeight - screen_h)/2 );
-		
-		X1 = X0 + screen_w;
-		Y1 = Y0 + screen_h;
-		
+	}else{
+		orient_now = "portrait";
+		PIXEL = Math.floor(window.innerWidth/N);
 	}
+	
+	while(1){
+		if(PIXEL%2===0)
+			break;
+		else
+			PIXEL--;			
+	}
+	
+	PIXEL = PIXEL/2;
+	N = 2*N;
+	
+	screen_w = N*PIXEL;
+	screen_h = N*PIXEL;
+	
+	X0 = Math.floor( (window.innerWidth - screen_w)/2 );
+	Y0 = Math.floor( (window.innerHeight - screen_h)/2 );
+	
+	X1 = X0 + screen_w;
+	Y1 = Y0 + screen_h;
+	
+	XC = Math.floor(window.innerWidth/2);
+	YC = Math.floor(window.innerHeight/2);
 	
 	console.log("WINDOW", window.innerWidth, "x", window.innerHeight);
 	console.log("PIXEL", PIXEL);
@@ -109,11 +113,15 @@ window.addEventListener('mousedown', mousedown, false);
 function mousedown(e){
 	
 	click_counter++;
-	console.log(click_counter);
-	
 	mouse = {x: e.clientX, y: e.clientY};
+	touch = {left: false, right: false};
 	
-	console.log("click", mouse.x, mouse.y);
+	if(mouse.x <= XC)
+		touch.left = true;
+	else
+		touch.right = true;
+	
+	// console.log("click", mouse.x, mouse.y);
 	
 }
 
@@ -150,28 +158,50 @@ function keyup(e) {
 
 function states(){
 	
-	if(state==="play"){
+	if(state==="welcome"){
 		
-		opt = { speed: PIXEL/2/2/2,
-				gravity: PIXEL/2/2
+		ctx.fillStyle = "gray";
+		ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+		ctx.font = 4*PIXEL+"px Verdana";
+		ctx.fillStyle = "black";
+		ctx.textAlign = "center";
+		ctx.fillText("M1N1G4M3...", window.innerWidth/2, window.innerHeight/2);
+		
+		if(touch.left || touch.right){
+			touch = {left: false, right: false};
+			openFullscreen();
+			state = "play";
 		}
 		
-		hero = new Hero(X0+2*PIXEL, Y1-2*PIXEL, 2*PIXEL, 2*PIXEL);
+	}
+	
+	if(state==="play"){
+		
+		opt = { speed: PIXEL/2/2/2 }
+		
+		console.log(opt);
+		
+		hero = new Hero(0, screen_h-2*PIXEL, 2*PIXEL, 2*PIXEL);
 		hero.lw = 0;
 		hero.lc = "black";
 		hero.fc = "blue";
+		hero.xdir = "right";
 		hero.dx = opt.speed;
 		
-		apple = new Apple(X0, Y0, PIXEL, PIXEL);
+		apple = new Apple(0, 0, PIXEL, PIXEL);
 		apple.fc = "red";
 		apple.lc = "black";
 		apple.lw = 0;
-		apple.dy = opt.speed;
+		apple.dy = 2*opt.speed;
+		apple.ydir = "down";
 		
 		apple_respawn();
 		
 		mobs = [];
 
+		mobs_spawn();
+		mobs_spawn();
 		mobs_spawn();
 		
 		score = 0;
@@ -182,6 +212,8 @@ function states(){
 	
 	
 	if(state==="playing"){
+		
+		// console.log(apple);
 		
 		if(frame%1===0){
 			
@@ -205,23 +237,19 @@ function states(){
 			
 			//collisions
 			
-			if(coll_rect_rect(hero, apple)){
-				apple_respawn();
-				score++;
-				console.log("SCORE: ", score);
-				mobs_spawn();
-			}
+			// if(coll_rect_rect(hero, apple)){
+				// apple_respawn();
+				// score++;
+				// console.log("SCORE: ", score);
+				// mobs_spawn();
+			// }
 			
-			for(let i=0; i<mobs.length; i++){
-				
-				if(coll_rect_rect(hero, mobs[i])){
-					
-					state = "play";
-					console.log("GAME OVER");
-					
-				}
-				
-			}
+			// for(let i=0; i<mobs.length; i++){
+				// if(coll_rect_rect(hero, mobs[i])){
+					// state = "play";
+					// console.log("GAME OVER");
+				// }
+			// }
 			
 			
 
@@ -253,10 +281,12 @@ class MyHero {
 }
 
 
-function Hero(x, y, w, h){
+function Hero(ix, iy, w, h){
 	
-	this.x = x;
-	this.y = y;
+	this.x = 0; //absoluto
+	this.y = 0; //absoluto
+	this.ix = ix; //relativo
+	this.iy = iy; //relativo
 	this.w = w;
 	this.h = h;
 	this.fc = "gray";
@@ -264,131 +294,62 @@ function Hero(x, y, w, h){
 	this.lw = 0;	
 	this.dx = 0;
 	this.dy = 0;
-	this.jumping = true;
-	this.jump = 0;
-	
+	this.xdir = "right";
+
 }
 
 function hero_update(){
 	
-	hero.dy += opt.gravity;
+	// limits
 	
-	hero.x += hero.dx;
-	hero.y += hero.dy;
-	
-	// console.log(hero.x);
-	
-	if(hero.x< X0){
-		// hero.x = X1;
-		hero.dx *= -1;
+	if(hero.ix <= 0){
+		hero.ix = 0;
+		hero.xdir = "right";
 	}
 
-	if(hero.x+hero.w > X1){
-		
-		hero.dx *= -1;	
-		hero.x = X1 - hero.w;
-	}
-
-	if(hero.y + hero.h >= Y1){
-		hero.y  = Y1 - hero.h;
-		hero.jumping = false;
-		// hero.jump = 0;
-		hero.dy = 0;
-		if(hero.dx>0)
-			hero.dx = opt.speed;
-		else
-			hero.dx = -opt.speed;
+	if(hero.ix+hero.w > screen_w){
+		hero.ix = screen_w - hero.w;
+		hero.xdir = "left";	
 	}
 	
-	if(mouse.x >= X1 && click_counter===1 ){
-		if(hero.dx<0)
-			hero.dx *= -1;
-		click_counter = 0;
-	}
+	//input, keyboard
 
-	if(mouse.x <= X0 && click_counter===1){
-		if(hero.dx>0)
-			hero.dx *= -1;		
-		click_counter = 0;
-	}
-
-	if(mouse.x > X0 && mouse.x < X1 && click_counter===1){
-		hero.dy -= hero.h;
-		if(hero.dx>0)
-			hero.dx = 4*opt.speed;
-		else
-			hero.dx = -4*opt.speed;
-		hero.jumping = true;
-		click_counter = 0;
-	}
-
-	if(key.ArrowRight && keycounter===1 && hero.dx<0){
-		hero.dx *= -1;
+	if(key.ArrowRight && keycounter===1){
+		hero.xdir = "right";
 		key.ArrowRight = false;
 	}
 	
-	if(key.ArrowLeft && keycounter===1 && hero.dx>0){
-		hero.dx *= -1;
-		key.ArrowRight = false;
-	}	
-	
-	if(key.ArrowUp && !hero.jumping && keycounter===1){
-		hero.dy -= hero.h;
-		if(hero.dx>0)
-			hero.dx = 4*opt.speed;
-		else
-			hero.dx = -4*opt.speed;
-		hero.jumping = true;
+	if(key.ArrowLeft && keycounter===1){
+		hero.xdir = "left";
 		key.ArrowRight = false;
 	}
+
+	//input, mouse, touch
+
+	if(touch.left){
+		hero.xdir = "left";
+		touch.left = false;
+	}
+
+	if(touch.right){
+		hero.xdir = "right";
+		touch.right = false;
+	}
+
+	//move
+	
+	if(hero.xdir==="right")
+		hero.ix += hero.dx;
+
+	if(hero.xdir==="left")
+		hero.ix -= hero.dx;
+
+	//absolute coord
+
+	hero.x = X0 + hero.ix;
+	hero.y = Y0 + hero.iy;
 
 }
-
-function hero_update_(){
-	
-	if(key.ArrowRight){
-		hero.x += PIXEL;
-	}
-
-	if(key.ArrowLeft){
-		hero.x -= PIXEL;
-	}
-
-	if(key.ArrowUp){
-		hero.y -= PIXEL;
-	}
-
-	if(key.ArrowDown){
-		hero.y += PIXEL;
-	}
-
-	if(key.d && keycounter===1){
-		hero.x += PIXEL;
-		key.d = false;
-	}
-
-	if(key.a && keycounter===1){
-		hero.x -= PIXEL;
-		key.a = false;
-	}
-
-	if(key.w && keycounter===1){
-		hero.y -= PIXEL;
-		key.w = false;
-	}
-
-	if(key.s && keycounter===1){
-		hero.y += PIXEL;
-		key.s = false;
-	}
-
-	if(hero.x <= X0)
-		hero.x = X0;
-
-	if(hero.x >= X1 - hero.w)
-		hero.x = X1 - hero.w;
-}
-
 
 
 function coll_rect_rect(A, B){
@@ -403,54 +364,66 @@ function coll_rect_rect(A, B){
 	}
 }
 
-function Apple(x, y, w, h){
+function Apple(ix, iy, w, h){
 	
-	this.x = x;
-	this.y = y;
+	this.x = 0; //absoluto
+	this.y = 0; //absoluto
+	this.ix = ix; //relativo
+	this.iy = iy; //relativo
 	this.w = w;
 	this.h = h;
 	this.fc = "gray";
 	this.lc = "black";
 	this.lw = 1;
 	this.dy = 1;	
+	this.ydir ="down";
 	
 }
 
 function apple_respawn(){
-	
-	
-	while(1){
-		apple.x = X0 + irand(1, N-1)*PIXEL;
-		apple.y = Y0 + apple.h;;
-		
-		if(!coll_rect_rect(hero, apple))
-			break;
-		
-	}
 
-	
+	apple.ix = irand(1, N-1)*PIXEL;
+	apple.iy = 0;
 
 }
 
 function apple_update(o){
-	
-	o.y += o.dy;
-	
-	if((o.y + o.h) >= Y1){
-		o.dy *= -1;
+
+	//limits
+
+	if( o.iy <= 0 ){
+		o.iy = 0;
+		o.ydir = "down";
 	}
 
-	if((o.y ) <= Y0){
-		o.dy *= -1;
+	if((o.iy + o.h) >= screen_h){
+		o.iy = screen_h - o.h;
+		o.ydir = "up";
 	}
+
+	//move
+	
+	if(o.ydir === "up")
+		o.iy -= o.dy;
+
+	if(o.ydir === "down")
+		o.iy += o.dy;
+
+	
+	//abs coord
+	
+	o.x = X0 + o.ix;
+	o.y = Y0 + o.iy;	
 
 }
 
 
-function Mob(x, y, w, h){
+function Mob(ix, iy, w, h){
 	
-	this.x = x;
-	this.y = y;
+	this.x = 0; //absoluto
+	this.y = 0; //absoluto
+	this.ix = ix; //relativo
+	this.iy = iy; //relativo
 	this.w = w;
 	this.h = h;
 	this.fc = "gray";
@@ -458,6 +431,8 @@ function Mob(x, y, w, h){
 	this.lw = 1;
 	this.dx = 1;
 	this.dy = 1;
+	this.xdir = "right";
+	this.ydir = "down";
 	
 }
 
@@ -470,48 +445,73 @@ function mobs_spawn(){
 	else
 		k = mobs.length;
 	
-	mobs.push( new Mob(X0+irand(1,N-1)*PIXEL, Y0+irand(1, N/2)*PIXEL, PIXEL, PIXEL) );
-	
-	if(irand(1,2)===1)
-		mobs[k].dx *= -1;
-	if(irand(1,2)===1)
-		mobs[k].dy *= -1;
-	
+	mobs.push( new Mob(irand(1, N-1)*PIXEL, irand(1, N-N/2)*PIXEL, PIXEL, PIXEL) );
+
 	mobs[k].lw = 0;
 	mobs[k].lc = "black";
 	mobs[k].fc = "black";
 	mobs[k].dx = opt.speed;
 	mobs[k].dy = opt.speed;
 	
+	if(irand(1,2)===1)
+		mobs[k].xdir = "right";
+	else
+		mobs[k].xdir = "left";
+
+	if(irand(1,2)===1)
+		mobs[k].ydir = "down";
+	else
+		mobs[k].ydir = "up";
+	
+
+	
 	//console.log(mobs[0]);
 
 }
 
 function mob_update(o){
-	
-	o.x += o.dx;
-	o.y += o.dy;
-	
-	if(o.x <= X0){
-		o.x = X0;
-		o.dx *= -1;
+
+	//limits
+
+	if( o.iy <= 0 ){
+		o.iy = 0;
+		o.ydir = "down";
 	}
 
-	if(o.x >= X1 - o.w){
-		o.x = X1 - o.w;
-		o.dx *= -1;	
+	if((o.iy + o.h) >= screen_h){
+		o.iy = screen_h - o.h;
+		o.ydir = "up";
 	}
 
-	if(o.y <= Y0){
-		o.y = Y0;
-		o.dy *= -1;
+	if( o.ix <= 0 ){
+		o.ix = 0;
+		o.xdir = "right";
 	}
 
-	if(o.y >= Y1 - o.h){
-		o.y = Y1 - o.h;
-		o.dy *= -1;	
+	if((o.ix + o.w) >= screen_w){
+		o.ix = screen_w - o.w;
+		o.xdir = "left";
 	}
 	
+	//move
+	
+	if(o.ydir === "up")
+		o.iy -= o.dy;
+
+	if(o.ydir === "down")
+		o.iy += o.dy;
+
+	if(o.xdir === "right")
+		o.ix += o.dx;
+
+	if(o.xdir === "left")
+		o.ix -= o.dx;
+	
+	//abs coord
+	
+	o.x = X0 + o.ix;
+	o.y = Y0 + o.iy;
+
 }
 
 function draw_screen(){
@@ -556,4 +556,64 @@ function draw_rect(r){
 
 function irand(min,max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+window.addEventListener('resize', resize, false);
+function resize(){
+	
+	get_hxw();
+	
+	ctx.canvas.width  = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border: 0px solid blue";
+	
+	//recargar PIXEL y speeds
+	
+	opt = { speed: PIXEL/2/2/2 }
+	
+	hero.w =2*PIXEL;
+	hero.h =2*PIXEL;
+	hero.dx = opt.speed;
+	hero.x = X0 + hero.ix;
+	hero.y = Y0 + hero.iy;
+	
+	apple.w = PIXEL;
+	apple.h = PIXEL;
+	apple.dy = 2*opt.speed;
+	apple.x = X0 + apple.ix;
+	apple.y = Y0 + apple.iy;
+	
+	for(let i=0; i<mobs.length; i++){
+		mobs[i].w = PIXEL;
+		mobs[i].h = PIXEL;
+		mobs[i].dx = opt.speed;
+		mobs[i].dy = opt.speed;
+		mobs[i].x = X0 + mobs[i].ix;
+		mobs[i].y = Y0 + mobs[i].iy;
+	}
+	
+}
+
+function openFullscreen() {
+	  if (document.documentElement.requestFullscreen) {
+		document.documentElement.requestFullscreen();
+	  } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
+		document.documentElement.mozRequestFullScreen();
+	  } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+		document.documentElement.webkitRequestFullscreen();
+	  } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
+		document.documentElement.msRequestFullscreen();
+	  }
+}
+
+function closeFullscreen() {
+	  if (document.exitFullscreen) {
+		document.exitFullscreen();
+	  } else if (document.mozCancelFullScreen) { /* Firefox */
+		document.mozCancelFullScreen();
+	  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+		document.webkitExitFullscreen();
+	  } else if (document.msExitFullscreen) { /* IE/Edge */
+		document.msExitFullscreen();
+	  }
 }
